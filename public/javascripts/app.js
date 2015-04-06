4394d28b5b5523acb644780d5a416d3a98c9b025
Christophe <christophe.guillonnet@gmail.com>
2015-04-06 19:20:18 +0200

    Archi Angular

---
+++ public/javascripts/app.js
@@ -0,0 +1,30 @@
+var app = angular.module('chat', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('main', {
+			url: '',
+			abtract: true,
+			templateUrl: 'views/main.html'
+		})
+
+		.state('login', {
+			parent: 'main',
+			url: '/login',
+			templateUrl: 'views/login.html',
+			controller: 'LoginController'
+		})
+
+		.state('messenger', {
+			parent: 'main',
+			templateUrl: 'views/messenger.html',
+			controller: 'MessengerController'
+		});
+});
+
+app.run(['$state', function ($state) {
+   $state.go('login');
+}]);
