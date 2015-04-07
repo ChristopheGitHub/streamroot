8dd191b30e3a98b4d810d51d9b49bcd6f0803827
Christophe <christophe.guillonnet@gmail.com>
2015-04-07 15:52:32 +0200

    Conversation establishment ok

--- public/javascripts/controller/MessengerController.js
+++ public/javascripts/controller/MessengerController.js
@@ -1,15 +1,24 @@
 app.controller('MessengerController', function ($scope, $stateParams, socket) {
 
 	$scope.user = $stateParams.user;
-
+	$scope.peer = $stateParams.peer;
 	$scope.query = "";
-
 	$scope.directory = null;
-
 	$scope.conversations = [];
 
 	console.log($scope.user.username);	
 	console.log($scope.user.peerId);
+	console.log($scope.peer);
+
+	var getUserFromPeerId = function(id){
+		var res = "";
+		angular.forEach($scope.directory, function(user){
+			if (user.peerId === id){
+				res = user.username;
+			}
+		});
+		return res;
+	};
 
 	socket.emit('getList', function() {
 		console.log("Get List");
@@ -33,10 +42,23 @@ app.controller('MessengerController', function ($scope, $stateParams, socket) {
 
 	$scope.createConversation = function(user){
 		console.log('createConversation');
+		var dataConnection = $scope.peer.connect(user.peerId);
 		var conversation = {
-			title: user.username
+			title: user.username,
+			dataConnection: dataConnection
 		};
 		$scope.conversations.push(conversation);
 	};
+
+	$scope.peer.on('connection', function(connection){
+		var user = getUserFromPeerId(connection.peer);
+		console.log('user ' + user + "want to start conv");
+		var conversation = {
+			title: user,
+			dataConnection: connection
+		};
+		$scope.$apply($scope.conversations.push(conversation));
+		console.log($scope.conversations);
+	});
 
 });
