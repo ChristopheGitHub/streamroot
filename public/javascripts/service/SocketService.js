f977a47ffcf5195380bf48bf9551c584c923d4f9
Christophe <christophe.guillonnet@gmail.com>
2015-04-09 11:59:38 +0200

    Change addresses for dedibox

--- public/javascripts/service/SocketService.js
+++ public/javascripts/service/SocketService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.factory('socket', function ($rootScope) {
-  var socket = io.connect('http://localhost:9000');
+  var socket = io.connect('http://sd-44839.dedibox.fr:9001');
   return {
     on: function (eventName, callback) {
       socket.on(eventName, function () {  
